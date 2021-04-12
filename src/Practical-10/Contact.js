ab969f4c3604bc05a659c2292797c4d80f0ee852
Unnati Goyal <unnatig412000@gmail.com>
2021-04-12 14:02:51 +0530

    practical modified

--- src/Practical-10/Contact.js
+++ src/Practical-10/Contact.js
@@ -4,22 +4,23 @@ export default function Contact(){
     return(
         <>
             <h2 className = "heading">Contact Page</h2>
-            {/* <form>
+            <form className = "form">
                 <div className="form-group">
                     <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                    {/* input and all tags needs to be closed for output in react as it works on html5 */}
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
                     <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
                 </div>
                 <div className="form-group">
                     <label for="exampleInputPassword1">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password">
+                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
                 </div>
                 <div className="form-group form-check">
-                    <input type="checkbox" className="form-check-input" id="exampleCheck1">
+                    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
                     <label className="form-check-label" for="exampleCheck1">Check me out</label>
                 </div>
                 <button type="submit" className="btn btn-primary">Submit</button>
-            </form> */}
+            </form>
         </>
     );
 }
